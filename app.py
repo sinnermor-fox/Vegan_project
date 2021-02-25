a453802da21113bcac3f56da88e42f4ac2de0621
sinnermor-fox <akordyukova.pronto@gmail.com>
2021-02-25 21:28:57 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:123456@localhost:5432/main_food"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+if __name__ == '__main__':
+    app.run()
